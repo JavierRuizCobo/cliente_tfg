f33adb1dd9a69b132bb58986d9a358f68fdcf555
JavierRuizCobo <javier.ruiz1@alumnos.uneatlantico.es>
2024-05-05 02:32:19 +0200

    Header funcional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { HabitosSaludablesComponent } from './habitos-saludables/habitos-saludables.component';
 import { SugerenciaPreguntaComponent } from './sugerencia-pregunta/sugerencia-pregunta.component';
 import { EjerciciosComponent } from './ejercicios/ejercicios.component';
 import { ListaUsuariosComponent } from './usuarios/usuarios.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent, HabitosSaludablesComponent, EjerciciosComponent, ListaUsuariosComponent],
+  imports: [RouterModule, HeaderComponent, HabitosSaludablesComponent, EjerciciosComponent, ListaUsuariosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+
+    { 
+      path: 'ejercicios', 
+      loadComponent: () => import('./ejercicios/ejercicios.component').then(m => m.EjerciciosComponent)
+    },
+    { 
+      path: 'usuarios', 
+      loadComponent: () => import('./usuarios/usuarios.component').then(m => m.ListaUsuariosComponent)
+    },
+    { 
+      path: 'habitossaludables', 
+      loadComponent: () => import('./habitos-saludables/habitos-saludables.component').then(m => m.HabitosSaludablesComponent)
+    }
+
+];
+
