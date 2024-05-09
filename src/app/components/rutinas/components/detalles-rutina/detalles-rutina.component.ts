c1b4b49d75dedadf070438245cb34b656505346d
JavierRuizCobo <javier.ruiz1@alumnos.uneatlantico.es>
2024-05-09 02:00:48 +0200

    creados componentes

---
+++ src/app/components/rutinas/components/detalles-rutina/detalles-rutina.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalles-rutina',
+  standalone: true,
+  imports: [],
+  templateUrl: './detalles-rutina.component.html',
+  styleUrl: './detalles-rutina.component.css'
+})
+export class DetallesRutinaComponent {
+
+}
