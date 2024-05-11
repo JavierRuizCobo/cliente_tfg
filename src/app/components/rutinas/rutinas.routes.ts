e628dbd417f317324e4d3289a9e1ec98a8535e11
JavierRuizCobo <javier.ruiz1@alumnos.uneatlantico.es>
2024-05-11 18:59:42 +0200

    Prueba routing

---
+++ src/app/components/rutinas/rutinas.routes.ts
@@ -0,0 +1,10 @@
+import { Routes, RouterModule } from '@angular/router';
+import { RutinasComponent } from './components/rutinas/rutinas.component';
+import { DetallesRutinaComponent } from './components/detalles-rutina/detalles-rutina.component';
+
+const routes: Routes = [
+  { path: '', component: RutinasComponent },
+  { path: 'detalle/:id', component: DetallesRutinaComponent }
+];
+
+export const RutinasRoutes = routes
